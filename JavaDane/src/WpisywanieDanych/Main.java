ea02975c0d8b2046352ff54535bf8ffa277f037b
Kosiarz101 <pol1100@o2.pl>
2020-07-02 16:56:56 +0200

    Here adding new functions to the system

---
+++ JavaDane/src/WpisywanieDanych/Main.java
@@ -0,0 +1,8 @@
+package WpisywanieDanych;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ JavaDane/src/WpisywanieDanych/Pracownik.java
@@ -0,0 +1,40 @@
+package WpisywanieDanych;
+
+public class Pracownik {
+    private String imieNazwisko;
+    private int wiek;
+    private String miasto;
+    private String zawod;
+
+    public String getImieNazwisko() {
+        return imieNazwisko;
+    }
+
+    public void setImieNazwisko(String imieNazwisko) {
+        this.imieNazwisko = imieNazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+
+    public String getMiasto() {
+        return miasto;
+    }
+
+    public void setMiasto(String miasto) {
+        this.miasto = miasto;
+    }
+
+    public String getZawod() {
+        return zawod;
+    }
+
+    public void setZawod(String zawod) {
+        this.zawod = zawod;
+    }
+}
